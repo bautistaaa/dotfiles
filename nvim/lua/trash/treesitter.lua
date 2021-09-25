53bd41ea35439b46ad92d4492698d4d8968ecf8a
Arnold Chand <creativenull@outlook.com>
2021-09-25 15:12:15 -0400

    feat: formatting

--- nvim/lua/trash/treesitter.lua
+++ nvim/lua/trash/treesitter.lua
@@ -1,5 +1,5 @@
-  require 'nvim-treesitter.configs'.setup {
-    ensure_installed = {
+require 'nvim-treesitter.configs'.setup {
+  ensure_installed = {
     'css',
     'graphql',
     'html',
@@ -9,8 +9,7 @@
     'python',
     'tsx',
     'typescript',
-    'svelte'
+    'svelte',
   },
-    highlight = { enable = true }
-  }
-
+  highlight = { enable = true },
+}
