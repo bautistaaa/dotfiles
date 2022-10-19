dfa25ca8d5ef71eb2cf91c47ff745eaaf8e90f47
Arnold Chand <creativenull@outlook.com>
2022-10-19 19:27:27 -0400

    feat: mv leader-p to null-ls

--- nvim/lua/trash/plugins/configs/lspconfig.lua
+++ nvim/lua/trash/plugins/configs/lspconfig.lua
@@ -16,9 +16,6 @@ local function on_attach(client, bufnr)
 	vim.keymap.set("n", "gW", vim.lsp.buf.workspace_symbol, { desc = "Search workspace symbols [LSP]", buffer = bufnr })
 	vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Show references [LSP]", buffer = bufnr })
 	vim.keymap.set("n", "<c-k>", vim.lsp.buf.signature_help, { desc = "Show signature help [LSP]", buffer = bufnr })
-	-- vim.keymap.set("n", "<leader>p", function()
-	-- 	vim.lsp.buf.format({ name = client.name, bufnr = bufnr, async = false, timeout_ms = 2500 })
-	-- end, { desc = "Format buffer [LSP]", buffer = bufnr })
 
 	-- LSP Saga keymaps
 	vim.keymap.set("n", "K", "<Cmd>Lspsaga hover_doc<CR>", { desc = "Hover documentation [LSP]", buffer = bufnr })
@@ -164,6 +161,10 @@ local diagnosticls = require("diagnosticls-configs")
 
 diagnosticls.init({
 	on_attach = function(_, bufnr)
+		vim.keymap.set("n", "<leader>p", function()
+			vim.lsp.buf.format({ name = "diagnosticls", bufnr = bufnr, async = false, timeout_ms = 2500 })
+		end, { desc = "Format buffer [LSP]", buffer = bufnr })
+
 		vim.api.nvim_clear_autocmds({ group = diagnosticls_group, buffer = bufnr })
 		vim.api.nvim_create_autocmd("BufWritePre", {
 			group = diagnosticls_group,
