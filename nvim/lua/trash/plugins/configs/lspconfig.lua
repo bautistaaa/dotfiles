671e2fa9801e57dcc80c48f61b56f1b2d3b84a39
bautistaaa <chrisbautistaaa@gmail.com>
2024-12-04 16:05:17 -0800

    setup gopls with lsp

--- nvim/lua/trash/plugins/configs/lspconfig.lua
+++ nvim/lua/trash/plugins/configs/lspconfig.lua
@@ -158,6 +158,8 @@ lspconfig.dockerls.setup(default_config)
 lspconfig.html.setup(default_config)
 lspconfig.jsonls.setup(default_config)
 lspconfig.yamlls.setup(default_config)
+lspconfig.gopls.setup(default_config)
+
 
 -- Tailwind CSS
 local tw_highlight = require("tailwind-highlight")
